33c46ecb068850ded29b5b9356de27da12cd3fdf
Vinicius Lohmann Kieling <c.viniciuslk@gmail.com>
2019-09-25 19:34:42 -0300

    Add spaces to alphabet

--- polybius.js
+++ polybius.js
@@ -6,7 +6,7 @@ const alphabet = [
   ["m", "n", "o", "p", "q", "r"],
   ["s", "t", "u", "v", "w", "x"],
   ["y", "z", "0", "1", "2", "3"],
-  ["4", "5", "6", "7", "8", "9"]
+  ["4", "5", "6", "7", "8", "9", " "]
 ];
 
 const rl = readline.createInterface({
@@ -14,38 +14,42 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const cript = () =>
+  rl.question("Qual palavra você quer codificar? ", sentence => {
+    let position = "";
+
+    [...sentence].forEach(item => {
+      alphabet.forEach((i, iIndex) => {
+        i.forEach((j, jIndex) => {
+          if (j === item.toLocaleLowerCase())
+            position += `${iIndex + 1}${jIndex + 1}`;
+        });
+      });
+    });
+
+    console.log(position);
+
+    rl.close();
+  });
+
+const decript = () =>
+  rl.question("Decodificar:  ", sentence => {
+    let word = "";
+
+    const newSentence = [...sentence].map(item => item - 1);
+
+    for (x = 0; x < newSentence.length; x += 2) {
+      word += alphabet[newSentence[x]][newSentence[x + 1]];
+    }
+
+    console.log(word);
+
+    rl.close();
+  });
+
 rl.question(
   "O que você deseja fazer? \n 1 - Codificar \n 2 - Decodificar \n Opcao desejada: ",
   sentence => {
-    sentence === "1"
-      ? rl.question("Qual palavra você quer codificar? ", sentence => {
-          let position = "";
-
-          [...sentence].forEach(item => {
-            alphabet.forEach((i, iIndex) => {
-              i.forEach((j, jIndex) => {
-                if (j === item.toLocaleLowerCase())
-                  position += `${iIndex + 1}${jIndex + 1}`;
-              });
-            });
-          });
-
-          console.log(position);
-
-          rl.close();
-        })
-      : rl.question("Decodificar:  ", sentence => {
-          let word = "";
-
-          const newSentence = [...sentence].map(item => item - 1);
-
-          for (x = 0; x < newSentence.length; x += 2) {
-            word += alphabet[newSentence[x]][newSentence[x + 1]];
-          }
-
-          console.log(word);
-
-          rl.close();
-        });
+    sentence === "1" ? cript() : decript();
   }
 );
